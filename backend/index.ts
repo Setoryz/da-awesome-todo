5f3f8fde126b163de63dee896833955ddbf95b19
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-20 22:23:13 +0100

    🎉 [backend] - typescript setup

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,8 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
 const PORT = 5000;
+
 app.get("/", (req, res) => res.send("Express + TypeScript Server"));
 app.listen(PORT, () => {
   console.log(`⚡️ Server running at https://localhost:${PORT}`);
