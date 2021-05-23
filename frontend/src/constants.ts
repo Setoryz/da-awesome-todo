e30b2b6a3f473cdf3d1a26ec22a3eee767f2d21c
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-23 21:59:13 +0100

    🚀 [frontend] - added backend deployed url -added favicon and changed title

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "http://localhost:5000/todos";
+// export const API_BASE_URL = "http://localhost:5000/todos";
+export const API_BASE_URL = "https://da-awesome-todo.herokuapp.com/todos";
