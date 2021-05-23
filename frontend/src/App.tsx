de835e3b3e05f3d8112d785df03b3d697c8a186f
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-23 07:48:12 +0100

    🎉 [frontend] - Added basic layout structure, styling, fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import "./App.scss";
+import TodoCalendar from "./components/TodoCalendar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* Title */}
+      <header>
+        <h1>Awesome Todo App</h1>
+      </header>
+
+      {/* calendar - week view */}
+      <TodoCalendar></TodoCalendar>
+      {/* calendar data */}
+    </div>
+  );
 }
 
 export default App;
