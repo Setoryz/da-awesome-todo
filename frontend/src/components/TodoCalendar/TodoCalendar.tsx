2cbbe8c2a5495ea9cbd5655115a623b6ee88c624
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-23 21:31:23 +0100

    ✨ [frontend] - Added month to TodoCalendar and additional styling

--- frontend/src/components/TodoCalendar/TodoCalendar.tsx
+++ frontend/src/components/TodoCalendar/TodoCalendar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { addDays, getDay, isSameDay } from "date-fns";
+import { addDays, format, getDay, isSameDay } from "date-fns";
 import "./TodoCalendar.scss";
 import TodoCalendarDate from "./TodoCalendarDate";
 import TodosColumn from "../TodosColumn/TodosColumn";
@@ -12,39 +12,44 @@ const TodoCalendar = () => {
   const [startDate, setStartDate] = useState(getStartDate(new Date()));
 
   return (
-    <div className="todo__calendar">
-      <div className="calendar__btn">
-        <button
-          className="btn__prev"
-          onClick={() => {
-            setStartDate((currentStartDate) => addDays(currentStartDate, -7));
-          }}
-        >
-          ◀
-        </button>
-        <button
-          className="btn__next"
-          onClick={() => {
-            setStartDate((currentStartDate) => addDays(currentStartDate, 7));
-          }}
-        >
-          ▶
-        </button>
+    <>
+      <div className="calendar__month">
+        <p>{format(startDate, "LLL")}</p>
       </div>
-      <div className="calendar__container">
-        {[...Array(7)].map((value, index) => (
-          <div key={index.toString()} className="calendar__column">
-            <TodoCalendarDate columnDate={addDays(startDate, index)} />
-            <TodosColumn
-              columnDate={addDays(startDate, index)}
-              todos={state.todos.filter(({ date }) =>
-                isSameDay(new Date(date), addDays(startDate, index))
-              )}
-            />
-          </div>
-        ))}
+      <div className="todo__calendar">
+        <div className="calendar__btn">
+          <button
+            className="btn__prev"
+            onClick={() => {
+              setStartDate((currentStartDate) => addDays(currentStartDate, -7));
+            }}
+          >
+            ◀
+          </button>
+          <button
+            className="btn__next"
+            onClick={() => {
+              setStartDate((currentStartDate) => addDays(currentStartDate, 7));
+            }}
+          >
+            ▶
+          </button>
+        </div>
+        <div className="calendar__container">
+          {[...Array(7)].map((value, index) => (
+            <div key={index.toString()} className="calendar__column">
+              <TodoCalendarDate columnDate={addDays(startDate, index)} />
+              <TodosColumn
+                columnDate={addDays(startDate, index)}
+                todos={state.todos.filter(({ date }) =>
+                  isSameDay(new Date(date), addDays(startDate, index))
+                )}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
