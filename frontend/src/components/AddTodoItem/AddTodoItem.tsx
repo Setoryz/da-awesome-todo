c36b8f3784ceb54c7a86f76f95d768d509d78d99
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-23 12:49:51 +0100

    ✨ [frontend] -Created Add TodoItem button

---
+++ frontend/src/components/AddTodoItem/AddTodoItem.tsx
@@ -0,0 +1,13 @@
+import AddIcon from "../icons/AddIcon";
+import "./AddTodoItem.scss";
+
+const AddTodoItem = () => {
+  return (
+    <div className="addTodoItem">
+      <span>Add Todo</span>
+      <AddIcon />
+    </div>
+  );
+};
+
+export default AddTodoItem;
