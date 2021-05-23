d80506736b4710808a2812bf3492b79ee786b26b
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-23 16:24:32 +0100

    ✨ [backend] - cors added

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import todosRoutes from "./routes/todos.routes";
+import cors from "cors";
 
 const app = express();
 const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
