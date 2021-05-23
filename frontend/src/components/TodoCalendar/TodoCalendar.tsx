042d0b9691853860e517aeb9ce672f74dc71070f
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-23 08:16:27 +0100

    ✨ [frontend] - TodoCalendar nav button and styling added

--- frontend/src/components/TodoCalendar/TodoCalendar.tsx
+++ frontend/src/components/TodoCalendar/TodoCalendar.tsx
@@ -10,12 +10,32 @@ const TodoCalendar = () => {
   const [startDate, setStartDate] = useState(getStartDate(new Date()));
   return (
     <div className="todo__calendar">
-      {[...Array(7)].map((value, index) => (
-        <div key={index.toString()} className="todo__calendar__column">
-          <TodoCalendarDate columnDate={addDays(startDate, index)} />
-          <TodosColumn columnDate={addDays(startDate, index)} />
-        </div>
-      ))}
+      <div className="calendar__btn">
+        <button
+          className="btn__prev"
+          onClick={() => {
+            setStartDate((currentStartDate) => addDays(currentStartDate, -7));
+          }}
+        >
+          ◀
+        </button>
+        <button
+          className="btn__next"
+          onClick={() => {
+            setStartDate((currentStartDate) => addDays(currentStartDate, 7));
+          }}
+        >
+          ▶
+        </button>
+      </div>
+      <div className="calendar__container">
+        {[...Array(7)].map((value, index) => (
+          <div key={index.toString()} className="calendar__column">
+            <TodoCalendarDate columnDate={addDays(startDate, index)} />
+            <TodosColumn columnDate={addDays(startDate, index)} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
